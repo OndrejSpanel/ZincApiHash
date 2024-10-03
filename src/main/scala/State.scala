b27ee66e1c372177f12782af798ec900f737b885
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2024-10-03 11:33:23 +0200

    Reduce repro

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -2,15 +2,10 @@
 
 object State {
 
-  // Provides works, which can be offered as actions for instance through ActionsAtHouseDoor
-  // examples: "send lumberjack to saw the log", "buy bread in the farmers shop"
   trait WorkProvider {
     def workStateIds: Seq[StateId]
   }
 
 }
 
-case class State(
-  entities: Entities,
-  timeUs: Long
-)
+case class State(entities: Entities)
